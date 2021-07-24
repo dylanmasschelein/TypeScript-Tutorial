8def6c7c9762eeed57b07a0108cd0f6d2c0cc50e
dylanmasschelein <dylanmasschelein@gmail.comclear>
2021-07-23 21:45:32 -0700

    Function types

--- understanding-typescript/app.js
+++ understanding-typescript/app.js
@@ -1,22 +1,14 @@
-function combine(input1, input2, resultConversion) {
-    var result;
-    if ((typeof input1 === "number" && typeof input2 === "number") ||
-        resultConversion === "as-number") {
-        result = +input1 + +input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    return result;
-    //   if (resultConversion === "as-number") {
-    //     return +result;
-    //   } else {
-    //     return result.toString();
-    //   }
+function add(n1, n2) {
+    return n1 + n2;
 }
-var combinedAges = combine(30, 26, "as-number");
-console.log(combinedAges);
-var combinedStringAges = combine("30", "26", "as-number");
-console.log(combinedStringAges);
-var combinedNames = combine("Max", "Anna", "as-text");
-console.log(combinedNames);
+function printResult(num) {
+    console.log("Result: " + num);
+}
+printResult(add(5, 12));
+// Accepts any function that takes 2 parameters and returns a number
+var combineValues;
+combineValues = add;
+// combineValues = printResult; - only declares it as a function so this workds
+// combineValues = 5; - not a function
+console.log(combineValues(8, 8));
+// let someValue: undefined;
