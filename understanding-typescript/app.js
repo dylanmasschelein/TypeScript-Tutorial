04c6aac3abbfccdba527dfca3d7d4bb34e463e09
dylanmasschelein <dylanmasschelein@gmail.comclear>
2021-07-23 21:23:07 -0700

    Understanding unions, aliases, Enums...

--- understanding-typescript/app.js
+++ understanding-typescript/app.js
@@ -1,8 +1,22 @@
-function add(n1, n2) {
-    console.log(typeof number1);
-    return n1 + n2;
+function combine(input1, input2, resultConversion) {
+    var result;
+    if ((typeof input1 === "number" && typeof input2 === "number") ||
+        resultConversion === "as-number") {
+        result = +input1 + +input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+    //   if (resultConversion === "as-number") {
+    //     return +result;
+    //   } else {
+    //     return result.toString();
+    //   }
 }
-var number1 = 5;
-var number2 = 2.8;
-var result = add(number1, number2);
-console.log(result);
+var combinedAges = combine(30, 26, "as-number");
+console.log(combinedAges);
+var combinedStringAges = combine("30", "26", "as-number");
+console.log(combinedStringAges);
+var combinedNames = combine("Max", "Anna", "as-text");
+console.log(combinedNames);
